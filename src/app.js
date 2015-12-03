fb0d82c946dc15abca3d4c0cf21d8c2b257f67bc
Sara Bee <sara.bainbridge@gmail.com>
2015-12-03 16:21:46 -0500

    :dog:umentation and cleanup

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ import PokeDex from './components/PokeDex.react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/*
+ * Wait until the DOM is loaded so that we have an element to render into.
+ * (The 'DOMContentLoaded' method is akin to $(document).ready(...) in jQuery.)
+ */
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <PokeDex />,
